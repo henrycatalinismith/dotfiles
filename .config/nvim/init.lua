62b286dc90ed430b8ed2c5ea5febdce644a9713a
Henry Catalini Smith <henry@catalinismith.se>
2021-03-19 20:42:06 +0100

    add nvim lua config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,26 @@
+vim.api.nvim_set_keymap(
+ "n",
+ "<space>q",
+ ":quit!<cr>",
+ {}
+)
+
+vim.api.nvim_set_keymap(
+ "n",
+ "<space>w",
+ ":write<cr>",
+ {}
+)
+
+vim.api.nvim_set_keymap(
+ "n",
+ "<space>wq",
+ ":write<cr>:quit!<cr>",
+ {}
+)
+
+vim.api.nvim_set_option(
+ "number",
+ true
+)
+
