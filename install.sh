b22204cfd96c8e4e423180f4ecde98533b602585
Henry Catalini Smith <henry@catalinismith.se>
2021-03-28 12:47:05 +0200

    finish ruby xdg

--- install.sh
+++ install.sh
@@ -12,19 +12,19 @@ dotfiles sparse-checkout set Library/Application\ Support/Code/User/settings.jso
 dotfiles sparse-checkout add .Brewfile
 dotfiles sparse-checkout add .alacritty.yml
 dotfiles sparse-checkout add .config/coc/extensions/package.json
+dotfiles sparse-checkout add .config/gem/gemrc
+dotfiles sparse-checkout add .config/irb/irbrc
 dotfiles sparse-checkout add .config/nvim/init.lua
 dotfiles sparse-checkout add .config/nvim/lua/plugins.lua
 dotfiles sparse-checkout add .config/nvim/coc-settings.json
+dotfiles sparse-checkout add .config/rails/railsrc
 dotfiles sparse-checkout add .config/tmux/plugins/tpm
 dotfiles sparse-checkout add .config/tmux/tmux.conf
 dotfiles sparse-checkout add .editorconfig
-dotfiles sparse-checkout add .gemrc
 dotfiles sparse-checkout add .gitconfig
 dotfiles sparse-checkout add .gitignore
 dotfiles sparse-checkout add .gitmodules
-dotfiles sparse-checkout add .irbrc
 dotfiles sparse-checkout add .nethackrc
-dotfiles sparse-checkout add .railsrc
 dotfiles sparse-checkout add .zshrc
 dotfiles checkout
 
