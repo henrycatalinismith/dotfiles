b924d8363215abb269c247959d48f9d37b4f293f
Henry Catalini Smith <henry@catalinismith.se>
2025-03-01 22:29:21 +0100

    Getting neovim running real nice now

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,29 +1,105 @@
+-------------------------------
+-- kb --------------------------
+--------------------------------
+
 -- Use space as the leader key
 --
 -- Space is the biggest key on
 -- a standard keyboard and
 -- doesn't do anything by
 -- default in normal mode.
-vim.g.mapleader = " "
+local function kb_set_leader()
+ vim.g.mapleader = " "
+end
 
-vim.keymap.set(
- "n",
- "<Tab>",
- ":tabnext<CR>",
- { noremap = true }
-)
-vim.keymap.set(
- "n",
- "<leader>q",
- ":q<CR>",
- { noremap = true }
-)
-vim.keymap.set(
- "n",
- "<leader>w",
- ":w<CR>",
- { noremap = true }
-)
+-- Map <leader>. to code_action
+--
+-- Intended to be a bit similar
+-- to <Cmd>. in VSCode.
+local function kb_map_code_action()
+ vim.keymap.set(
+  "n",
+  "<leader>.",
+  vim.lsp.buf.code_action,
+  {}
+ )
+end
+
+local function kb_map_move_line()
+ vim.keymap.set(
+  "n",
+  "<A-Down>",
+  ":m .+1<CR>==",
+  { noremap = true, silent = true }
+ )
+ vim.keymap.set(
+  "n",
+  "<A-Up>",
+  ":m .-2<CR>==",
+  { noremap = true, silent = true }
+ )
+ vim.keymap.set(
+  "v",
+  "<A-Down>",
+  ":m '>+1<CR>gv=gv",
+  { noremap = true, silent = true }
+ )
+ vim.keymap.set(
+  "v",
+  "<A-Up>",
+  ":m '<-2<CR>gv=gv",
+  { noremap = true, silent = true }
+ )
+ vim.keymap.set(
+  "i",
+  "<A-Down>",
+  "<Esc>:m .+1<CR>==gi",
+  { noremap = true, silent = true }
+ )
+ vim.keymap.set(
+  "i",
+  "<A-Up>",
+  "<Esc>:m .-2<CR>==gi",
+  { noremap = true, silent = true }
+ )
+end
+
+-- Map <leader>q to quit
+--
+-- Tend to go back and forth on
+-- whether or not to have ! on
+-- the end of this.
+local function kb_map_quit()
+ vim.keymap.set(
+  "n",
+  "<leader>q",
+  ":q<CR>",
+  { noremap = true }
+ )
+end
+
+-- Map <leader>w to write
+local function kb_map_write()
+ vim.keymap.set(
+  "n",
+  "<leader>w",
+  ":w<CR>",
+  { noremap = true }
+ )
+end
+
+-- Map tab to tabnext
+--
+-- Such a common movement that
+-- it deserves its own key.
+local function kb_map_tabnext()
+ vim.keymap.set(
+  "n",
+  "<Tab>",
+  ":tabnext<CR>",
+  { noremap = true }
+ )
+end
 
 --------------------------------
 -- ui --------------------------
@@ -91,13 +167,224 @@ local function ui_show_tabline()
  vim.o.showtabline = 2
 end
 
+--------------------------------
+-- pl --------------------------
+--------------------------------
+
+local function pl_barbar()
+ return {
+  "romgrk/barbar.nvim",
+  dependencies = {
+   "lewis6991/gitsigns.nvim",
+   "nvim-tree/nvim-web-devicons",
+  },
+ }
+end
+
+local function pl_cmp()
+ return {
+  "hrsh7th/nvim-cmp",
+ }
+end
+
+local function pl_cmp_lsp()
+ return {
+  "hrsh7th/cmp-nvim-lsp",
+  config = function()
+   require("cmp").setup({
+    sources = {
+     { name = "nvim_lsp" },
+    }
+   })
+  end,
+ }
+end
+
+local function pl_copilot()
+ return {
+  "github/copilot.vim",
+ }
+end
+
+local function pl_lspconfig()
+ return {
+  "neovim/nvim-lspconfig",
+  config = function()
+   kb_map_code_action()
+  end,
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+  }
+ }
+end
+
+local function pl_lualine()
+ return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = {
+   "nvim-tree/nvim-web-devicons"
+  },
+  config = function()
+   require('lualine').setup()
+  end
+ }
+end
+
+local function pl_mason()
+ return {
+  "williamboman/mason.nvim",
+  config = function()
+   require("mason").setup()
+  end,
+ }
+end
+
+local function pl_mason_lspconfig()
+ return {
+  "williamboman/mason-lspconfig.nvim",
+  config = function()
+   require("mason-lspconfig").setup()
+   require("lspconfig").lua_ls.setup {}
+   require("lspconfig").ts_ls.setup {}
+  end,
+  dependencies = {
+   "williamboman/mason.nvim",
+   "neovim/nvim-lspconfig",
+  }
+ }
+end
+
+local function pl_tailwind_tools()
+ return {
+  "luckasRanarison/tailwind-tools.nvim",
+  name = "tailwind-tools",
+  build = ":UpdateRemotePlugins",
+  dependencies = {
+   "nvim-treesitter/nvim-treesitter",
+   "nvim-telescope/telescope.nvim",
+   "neovim/nvim-lspconfig",
+  },
+  opts = {
+   cmp = {
+    highlight = "foreground",
+   },
+   document_color = {
+    enabled = true
+   }
+  }
+ }
+end
+
+local function pl_telescope()
+ return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+   "nvim-lua/plenary.nvim",
+  },
+ }
+end
+
+local function pl_telescope_ui_select()
+ return {
+  "nvim-telescope/telescope-ui-select.nvim",
+  config = function()
+   require("telescope").setup({
+    extensions = {
+     ["ui-select"] = {
+      require("telescope.themes").get_dropdown {}
+     }
+    }
+   })
+   require("telescope").load_extension("ui-select")
+  end
+ }
+end
+
+--------------------------------
+-- lz --------------------------
+--------------------------------
+
+-- Load lazy.nvim
+--
+-- Based on the single file
+-- setup instructions from
+-- https://github.com/folke/lazy.nvim
+local function lz_load()
+ local r = "https://github.com/folke/lazy.nvim.git"
+ local d = vim.fn.stdpath("data")
+ local l = "/lazy/lazy.nvim"
+ local lazypath = d .. l
+ local exists = vim.fn.isdirectory(r)
+ if not exists then
+  local out = vim.fn.system({
+   "git",
+   "clone",
+   "--filter=blob:none",
+   "--branch=stable",
+   r,
+   lazypath
+  })
+  if vim.v.shell_error ~= 0 then
+   vim.api.nvim_echo({
+    {
+     "Failed to clone lazy.nvim:\n",
+     "ErrorMsg",
+    },
+    {
+     out,
+     "WarningMsg",
+    },
+    {
+     "\nPress any key to exit...",
+    },
+   }, true, {})
+   vim.fn.getchar()
+   os.exit(1)
+  end
+ end
+ vim.opt.rtp:prepend(lazypath)
+end
+
+local function lz_spec()
+ return {
+  pl_barbar(),
+  pl_cmp(),
+  pl_cmp_lsp(),
+  pl_copilot(),
+  pl_lspconfig(),
+  pl_lualine(),
+  pl_mason(),
+  pl_mason_lspconfig(),
+  pl_tailwind_tools(),
+  pl_telescope(),
+  pl_telescope_ui_select(),
+ }
+end
+
+-- Setup lazy.nvim
+local function lz_setup()
+ require("lazy").setup({
+  spec = lz_spec()
+ })
+end
+
 local function init()
+ kb_set_leader()
+ kb_map_move_line()
+ kb_map_quit()
+ kb_map_write()
+ kb_map_tabnext()
+
  ui_enable_guicolors()
  ui_hide_tildes()
  ui_show_cursorline()
  ui_show_numbers()
  ui_show_signcolumn()
  ui_show_tabline()
+
+ lz_load()
+ lz_setup()
 end
 
 init()
