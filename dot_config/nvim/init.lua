37ebc5bd31cb306a8998039db256578c23b25141
Henry Catalini Smith <henry@catalinismith.se>
2025-09-24 22:04:41 +0200

    filetypes

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -3,6 +3,19 @@
 --------------------------------
 
 vim.api.nvim_create_autocmd(
+ {
+  "BufRead",
+  "BufNewFile",
+ },
+ {
+  pattern = "*.njk",
+  callback = function()
+   vim.bo.filetype = "html"
+  end,
+ }
+)
+
+vim.api.nvim_create_autocmd(
  "CursorHold",
  {
   callback = function()
@@ -52,6 +65,21 @@ vim.api.nvim_create_autocmd(
    vim.opt_local.shiftwidth = 2
    vim.opt_local.tabstop = 2
    vim.opt_local.softtabstop = 2
+   vim.opt_local.expandtab = true
+  end,
+ }
+)
+
+vim.api.nvim_create_autocmd(
+ "FileType",
+ {
+  pattern = {
+   "perl",
+  },
+  callback = function()
+   vim.opt_local.shiftwidth = 4
+   vim.opt_local.tabstop = 4
+   vim.opt_local.softtabstop = 4
    vim.opt_local.expandtab = true
   end,
  }
