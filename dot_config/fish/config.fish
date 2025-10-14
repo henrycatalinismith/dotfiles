e326d9d5a9d9f984a9d6a28f1fe83ee918273e8f
Henry Catalini Smith <henry@catalinismith.se>
2025-10-14 17:53:27 +0200

    editor

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -6,27 +6,28 @@ fish_add_path $HOME/.docker/bin
 fish_add_path $HOME/.local/bin
 
 function fish_greeting
-  clear
-  phoon | lolcat
+    clear
+    phoon | lolcat
 end
 
 function fish_prompt
-  set_color 92a1a1
-  echo -n '$ '
-  set_color normal
+    set_color 92a1a1
+    echo -n '$ '
+    set_color normal
 end
 
 if status is-interactive
-  atuin init fish | source
+    atuin init fish | source
 end
 
 function zellij_tab_name_update --on-event fish_preexec
-  if set -q ZELLIJ
-    set title (string split ' ' $argv)[1]
-    command nohup zellij action rename-tab $title >/dev/null 2>&1
-  end
+    if set -q ZELLIJ
+        set title (string split ' ' $argv)[1]
+        command nohup zellij action rename-tab $title >/dev/null 2>&1
+    end
 end
 
+set -Ux EDITOR hx
 set -Ux GEMRC ~/.config/gem/gemrc
 set -Ux IRBRC ~/.config/irb/irbrc
 set -Ux GIT_CONFIG_GLOBAL ~/.config/git/config
